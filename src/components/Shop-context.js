0a9e249f4c1ad16d5e1b64153edc2e60e6b25c43
pkm0707 <mpraveenkumar@student.tce.edu>
2023-11-26 22:34:45 +0530

    initial commited

--- src/components/Shop-context.js
+++ src/components/Shop-context.js
@@ -2,7 +2,7 @@ import { createContext,useState } from "react";
 import { products } from "./Products";
 export const ShopContext = createContext(null);
 
-const getDefaultCart = () => {
+function getDefaultCart () {
   let cart = {};
   for (let i = 1; i < products.length + 1; i++) {
     cart[i] = 0;
